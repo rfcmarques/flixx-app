dcc71c27836a41c9d36f8593591c4fcf2602d5f1
Rui Marques <a.marques1967@gmail.com>
2024-03-03 16:14:26 +0000

    Spinner functions and popular shows function

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,16 +32,58 @@ async function displayPopularMovies() {
     })
 }
 
+async function displayPopularShows() {
+    const { results } = await fetchAPIData('tv/popular');
+
+    results.forEach(show => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${show.poster_path ? `<img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="${show.name}"
+              />` : `<img
+              src="assets/images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>`;
+
+        document.querySelector('#popular-shows').appendChild(div);
+    })
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = 'd3340bbf9a1205a44ba7179701a1a074';
     const API_URL = 'https://api.themoviedb.org/3/';
 
-    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    showSpinner();
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=pt-PT`);
 
     const data = await response.json();
 
+    hideSpinner();
+
     return data;
+}
+
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
 }
 
 //Highlight active link
@@ -62,7 +104,7 @@ function init() {
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('movie details');
