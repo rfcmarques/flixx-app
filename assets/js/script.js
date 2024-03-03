4038d93dfde9f0ecf856307c96956ccde9c3b34c
Rui Marques <a.marques1967@gmail.com>
2024-03-03 16:37:22 +0000

    Fetch movie details

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,65 @@ async function displayPopularShows() {
     })
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+    const movieID = window.location.search.split('=')[1];
+
+    const movie = await fetchAPIData(`movie/${movieID}`);
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `<div class="details-top">
+    <div>
+    ${movie.poster_path ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />` : `<img
+  src="assets/images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+        }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        atque molestiae error debitis provident dolore hic odit, impedit
+        sint, voluptatum consectetur assumenda expedita perferendis
+        obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
+        quae molestiae cupiditate modi libero dolorem commodi obcaecati!
+        Ratione quia corporis recusandae delectus perspiciatis consequatur
+        ipsam. Cumque omnis ad recusandae.
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $ ${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $ ${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}</div>
+  </div>`;
+
+    document.querySelector('#movie-details').appendChild(div);
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = 'd3340bbf9a1205a44ba7179701a1a074';
@@ -96,6 +155,10 @@ function higlightActiveLink() {
     })
 }
 
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Init function
 function init() {
     switch (state.currentPage) {
@@ -107,7 +170,7 @@ function init() {
             displayPopularShows();
             break;
         case '/movie-details.html':
-            console.log('movie details');
+            displayMovieDetails();
             break;
         case '/tv-details.html':
             console.log('tv details');
